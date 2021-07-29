d8500c2f864962bf8dcdffc361f962fe3a54f6c0
s-ieremenko <ieremenko.sveta@gmail.com>
2021-07-29 10:53:31 +0300

    removed a comment

--- src/Role/role.service.ts
+++ src/Role/role.service.ts
@@ -31,7 +31,6 @@ export const createRoleWithPermissions = async (
     uuid: uuidv4(),
     name,
   });
-  // await role.$add('permissions', permissions);
   //@ts-ignore
   await role.addPermissions(permissions);
 };
